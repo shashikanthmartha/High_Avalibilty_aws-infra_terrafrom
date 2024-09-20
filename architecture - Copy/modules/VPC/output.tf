5f5ae02dd42694c724b15ce0a0f2edccc02814e0
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-20 21:22:58 +0530

    Update output.tf

--- architecture - Copy/modules/VPC/output.tf
+++ architecture - Copy/modules/VPC/output.tf
@@ -2,8 +2,14 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 output "public_subnets" {
-  value = aws_subnet.public.id
+   value = { for subnet in aws_subnet.public : subnet.id => {
+    id = subnet.id
+    
+  }}
 }
 output "private_subnets" {
-  value = aws_subnet.private.id
+  value = { for subnet in aws_subnet.private : subnet.id => {
+    id = subnet.id
+    
+  }}
 }
