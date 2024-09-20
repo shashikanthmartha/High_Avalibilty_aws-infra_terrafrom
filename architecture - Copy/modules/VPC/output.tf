eed2728aa6eb480869319e246e30f1472d389f4c
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-20 21:13:31 +0530

    Update output.tf

--- architecture - Copy/modules/VPC/output.tf
+++ architecture - Copy/modules/VPC/output.tf
@@ -1,9 +1,10 @@
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 output "public_subnets" {
-  value = aws_subnet.public.*.id
+  value = aws_subnet.public.arn
 }
 output "private_subnets" {
-  value = zipmap(aws_subnet.private.*.id, aws_subnet.private.*.id)
+  value = aws_subnet.private.arn
 }
