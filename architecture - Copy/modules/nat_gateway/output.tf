21a4df7aa38f4d1dfa6c2ed0f00db1d14292699c
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-20 22:36:08 +0530

    Update output.tf

--- architecture - Copy/modules/nat_gateway/output.tf
+++ architecture - Copy/modules/nat_gateway/output.tf
@@ -1,7 +1,4 @@
 output "nat_gateway_ids" {
-  # value = aws_nat_gateway.nat.*.id
-   value = { for nat in aws_nat_gateway.nat : nat.id => {
-    id = nat.id
-    
-  }}
+  
+   value = [for nat in aws_nat_gateway.nat : nat.id]
 }
