78096518737ef09458d2b55ce6d9608204a9045c
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-23 18:12:40 +0530

    Update main.tf

--- architecture - Copy/modules/autoscaling/main.tf
+++ architecture - Copy/modules/autoscaling/main.tf
@@ -136,8 +136,24 @@ resource "aws_security_group" "alb_sg" {
   name        = "${var.env}-alb-sg"
   description = "Security group for the application load balancer"
   vpc_id      = var.vpc_id
-  ingress = var.alb_sg_ingress_rules
-  egress = var.alb_sg_egress_rules
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "${var.env}-ALBSecurityGroup"
   }
@@ -180,8 +196,24 @@ resource "aws_security_group" "ec2_sg" {
   name        = "${var.env}-ec2-sg"
   description = "Security group for EC2 instances in the ASG"
   vpc_id      = var.vpc_id
-  ingress = var.ec2_sg_ingress_rules
-  egress = var.ec2_sg_egress_rules
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 
   tags = {
     Name = "${var.env}-EC2SecurityGroup"
