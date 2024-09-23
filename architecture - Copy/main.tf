a143d7956d86b448d9bb2006144033b4afb98840
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-23 16:00:23 +0530

    Update main.tf

--- architecture - Copy/main.tf
+++ architecture - Copy/main.tf
@@ -58,28 +58,22 @@ data "template_file" "user_data" {
   efs_file_system_id = module.efs.efs_file_system_id
   }
 }
-# module "rout53" {
-#     source = "./modules/ROUTE_53"
-#     app_alb_dns_name=module.auto_scaling.app_alb_dns_name
-#     app_alb_zone_id=module.auto_scaling.app_alb_zone_id
-# }
-
-# module "auto_scaling" {
-#     source = "./modules/autoscaling"
-#     env = var.env
-#     local_ssm_policies = local.ssm_policies
-#     ami_id = var.ami_id
-#     instance_type = var.instance_type
-#     auto_private_subnets = module.vpc.private_subnets
-#     auto_public_subnets = module.vpc.public_subnets
-#     vpc_id = module.vpc.vpc_id
-#     aws_acm_certificate_cert_arn = var.aws_acm_certificate_cert_arn#mdules.rout53
-#     alb_sg_ingress_rules = var.alb_sg_ingress_rules
-#     alb_sg_egress_rules = var.alb_sg_egress_rules
-#     user_data = data.template_file.user_data
-#     ec2_sg_ingress_rules = var.ec2_sg_ingress_rules
-#     ec2_sg_egress_rules = var.ec2_sg_egress_rules
-# }
+module "auto_scaling" {
+    source = "./modules/autoscaling"
+    env = var.env
+    local_ssm_policies = local.ssm_policies
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    auto_private_subnets = module.vpc.private_subnets
+    auto_public_subnets = module.vpc.public_subnets
+    vpc_id = module.vpc.vpc_id
+    aws_acm_certificate_cert_arn =module.rout53.aws_acm_certificate_cert_arn
+    alb_sg_ingress_rules = var.alb_sg_ingress_rules
+    alb_sg_egress_rules = var.alb_sg_egress_rules
+    user_data = data.template_file.user_data
+    ec2_sg_ingress_rules = var.ec2_sg_ingress_rules
+    ec2_sg_egress_rules = var.ec2_sg_egress_rules
+}
 # module "waf" {
 #  source = "./modules/WAF"
 #  env = var.env
